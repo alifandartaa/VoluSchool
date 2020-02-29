73cabd58363d5bc9c7981be6013adde91aef5525
Alif Andarta <alifandartaa@yahoo.com>
2020-02-29 21:41:03 +0700

    otw binding

--- app/src/main/java/com/example/voluschool/activities/DetailAplikasiActivity.java
+++ app/src/main/java/com/example/voluschool/activities/DetailAplikasiActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.voluschool.R;
 
-public class TopUpInfoActivity extends AppCompatActivity {
+public class DetailAplikasiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_top_up_info);
+        setContentView(R.layout.activity_detail_aplikasi);
     }
 }
---
+++ app/src/main/java/com/example/voluschool/responses/DonationPostResponse.java
@@ -0,0 +1,4 @@
+package com.example.voluschool.responses;
+
+public class DonationPostResponse {
+}
