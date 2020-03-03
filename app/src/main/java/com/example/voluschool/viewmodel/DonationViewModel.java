3b5a79764f8b98c62f0a13eb2575c87e692bbc7d
Alif Andarta <alifandartaa@yahoo.com>
2020-03-03 08:25:17 +0700

    tes

--- app/src/main/java/com/example/voluschool/viewmodel/DonationViewModel.java
+++ app/src/main/java/com/example/voluschool/viewmodel/DonationViewModel.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 
 public class DonationViewModel extends ViewModel {
     MyApi myApi;
+    int test = 0;
     private MutableLiveData<ArrayList<PostDonation>> listPostDonation = new MutableLiveData<>();
 
     public DonationViewModel(){
